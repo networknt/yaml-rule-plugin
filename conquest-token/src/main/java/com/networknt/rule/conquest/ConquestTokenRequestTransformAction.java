cfd8aeec22d25806a1d579ccb33c6927ce3d17fd
Steve Hu <stevehu@gmail.com>
2022-10-31 15:41:46 -0400

    fixes #10 add conquest planning token plugin action implementation

---
+++ conquest-token/src/main/java/com/networknt/rule/conquest/ConquestTokenRequestTransformAction.java
@@ -0,0 +1,216 @@
+package com.networknt.rule.conquest;
+
+import com.networknt.client.ClientConfig;
+import com.networknt.client.Http2Client;
+import com.networknt.client.oauth.TokenResponse;
+import com.networknt.client.ssl.TLSConfig;
+import com.networknt.config.JsonMapper;
+import com.networknt.config.TlsUtil;
+import com.networknt.monad.Failure;
+import com.networknt.monad.Result;
+import com.networknt.monad.Success;
+import com.networknt.proxy.PathPrefixAuth;
+import com.networknt.proxy.conquest.ConquestConfig;
+import com.networknt.rule.IAction;
+import com.networknt.rule.RuleActionValue;
+import com.networknt.rule.RuleConstants;
+import com.networknt.status.Status;
+import com.networknt.utility.HashUtil;
+import io.undertow.util.HeaderMap;
+import io.undertow.util.HeaderValues;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * It is called from the request transform interceptor from the light-gateway to get the conquest planning API access
+ * token to put into the Authorization header in order to access the conquest planning APIs. For the original consumer,
+ * it might have another token in the Authorization header for the gateway to verify in order to invoke the external
+ * service handler. Once the verification is done, the Authorization header will be replaced with the conquest token
+ * from the cache or retrieved from the conquest if it is expired or about to expire.
+ *
+ * @author Steve Hu
+ */
+public class ConquestTokenRequestTransformAction implements IAction {
+    private static final Logger logger = LoggerFactory.getLogger(ConquestTokenRequestTransformAction.class);
+    private ConquestConfig config;
+    private HttpClient client;
+
+    public ConquestTokenRequestTransformAction() {
+        config = ConquestConfig.load();
+        if(logger.isInfoEnabled()) logger.info("ConquestTokenRequestTransformAction is loaded.");
+    }
+
+    @Override
+    public void performAction(Map<String, Object> objMap, Map<String, Object> resultMap, Collection<RuleActionValue> actionValues) {
+        resultMap.put(RuleConstants.RESULT, true);
+        String requestPath = (String)objMap.get("requestPath");
+        if(logger.isTraceEnabled()) logger.trace("requestPath = " + requestPath);
+        for(PathPrefixAuth pathPrefixAuth: config.getPathPrefixAuths()) {
+            if(requestPath.startsWith(pathPrefixAuth.getPathPrefix())) {
+                if(logger.isTraceEnabled()) logger.trace("found with requestPath = " + requestPath + " prefix = " + pathPrefixAuth.getPathPrefix());
+                if(System.currentTimeMillis() >= (pathPrefixAuth.getExpiration())) {
+                    // get a new access token.
+                    String jwt = null;
+                    try {
+                        jwt = createJwt(pathPrefixAuth.getAuthIssuer(), pathPrefixAuth.getAuthSubject(), pathPrefixAuth.getAuthAudience(), HashUtil.generateUUID(), pathPrefixAuth.getTokenTtl());
+                    } catch (Exception e) {
+                        logger.error("Exception", e);
+                        return;
+                    }
+                    if(jwt != null) {
+                        // use the jwt to get the access token.
+                        TokenResponse tokenResponse = getAccessToken(pathPrefixAuth.getTokenUrl(), jwt);
+                        if(tokenResponse != null) {
+                            pathPrefixAuth.setExpiration(System.currentTimeMillis() + tokenResponse.getExpiresIn() * 1000 - 60000);
+                            pathPrefixAuth.setAccessToken(tokenResponse.getAccessToken());
+                        } else {
+                            return;
+                        }
+                    }
+                }
+                // either a new token is retrieved or cached token is not expired. Put the token into the Authorization header.
+                if(pathPrefixAuth.getAccessToken() != null) {
+                    Map<String, Object> requestHeaders = new HashMap<>();
+                    Map<String, Object> updateMap = new HashMap<>();
+                    updateMap.put("Authorization", "Bearer " + pathPrefixAuth.getAccessToken());
+                    requestHeaders.put("update", updateMap);
+                    resultMap.put("requestHeaders", requestHeaders);
+                    return;
+                }
+            }
+        }
+    }
+
+    private String createJwt(String issuer, String subject, String audience, String jti, int tokenTtl) throws Exception {
+        String certFileName = config.getCertFilename();
+        String certPassword = config.getCertPassword();
+
+        String header = "{\"typ\":\"JWT\", \"alg\":\"RS256\"}";
+        String claimTemplate = "'{'\"iss\": \"{0}\", \"sub\": \"{1}\", \"aud\": \"{2}\", \"jti\": \"{3}\", \"iat\": {4}, \"exp\": {5}'}'";
+        StringBuffer token = new StringBuffer();
+        // Encode the JWT Header and add it to our string to sign
+        token.append(org.apache.commons.codec.binary.Base64.encodeBase64URLSafeString(header.getBytes("UTF-8")));
+        // Separate with a period
+        token.append(".");
+
+        String[] claimArray = new String[6];
+        claimArray[0] = issuer;
+        claimArray[1] = subject;
+        claimArray[2] = audience;
+        claimArray[3] = jti;
+        claimArray[4] = Long.toString(( System.currentTimeMillis()/1000 ));
+        claimArray[5] = Long.toString(( System.currentTimeMillis()/1000 ) + tokenTtl);
+
+        MessageFormat claims;
+        claims = new MessageFormat(claimTemplate);
+        String payload = claims.format(claimArray);
+
+        // Add the encoded claims object
+        token.append(org.apache.commons.codec.binary.Base64.encodeBase64URLSafeString(payload.getBytes("UTF-8")));
+
+        KeyStore keystore = TlsUtil.loadKeyStore(certFileName, certPassword.toCharArray());
+        PrivateKey privateKey = (PrivateKey) keystore.getKey(certFileName.substring(0, certFileName.indexOf(".")), certPassword.toCharArray());
+
+        // Sign the JWT Header + "." + JWT Claims Object
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(privateKey);
+        signature.update(token.toString().getBytes("UTF-8"));
+        String signedPayload = org.apache.commons.codec.binary.Base64.encodeBase64URLSafeString(signature.sign());
+
+        // Separate with a period
+        token.append(".");
+
+        // Add the encoded signature
+        token.append(signedPayload);
+        return token.toString();
+    }
+
+    private TokenResponse getAccessToken(String serverUrl, String jwt) {
+        TokenResponse tokenResponse = null;
+        if(client == null) {
+            try {
+                HttpClient.Builder clientBuilder = HttpClient.newBuilder()
+                        .followRedirects(HttpClient.Redirect.NORMAL)
+                        .connectTimeout(Duration.ofMillis(ClientConfig.get().getTimeout()))
+                        .sslContext(Http2Client.createSSLContext());
+                if(config.getProxyHost() != null) clientBuilder.proxy(ProxySelector.of(new InetSocketAddress(config.getProxyHost(), config.getProxyPort() == 0 ? 443 : config.getProxyPort())));
+                if(config.isEnableHttp2()) clientBuilder.version(HttpClient.Version.HTTP_2);
+                // this a workaround to bypass the hostname verification in jdk11 http client.
+                Map<String, Object> tlsMap = (Map<String, Object>)ClientConfig.get().getMappedConfig().get(Http2Client.TLS);
+                if(tlsMap != null && !Boolean.TRUE.equals(tlsMap.get(TLSConfig.VERIFY_HOSTNAME))) {
+                    final Properties props = System.getProperties();
+                    props.setProperty("jdk.internal.httpclient.disableHostnameVerification", Boolean.TRUE.toString());
+                }
+                client = clientBuilder.build();
+
+            } catch (IOException e) {
+                logger.error("Cannot create HttpClient:", e);
+                return null;
+            }
+        }
+        try {
+            if(serverUrl == null) {
+                logger.error("tokenUrl is null");
+                return null;
+            }
+
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("grant_type", "client_credentials");
+            parameters.put("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
+            parameters.put("client_assertion", jwt);
+
+            String form = parameters.entrySet()
+                    .stream()
+                    .map(e -> e.getKey() + "=" + URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8))
+                    .collect(Collectors.joining("&"));
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(serverUrl))
+                    .headers("Content-Type", "application/x-www-form-urlencoded")
+                    .POST(HttpRequest.BodyPublishers.ofString(form))
+                    .build();
+
+            HttpResponse<?> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            // {"access_token":"eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FIiwia2lkIjoiM0ptbzhUWFJtQTJ2U2hkcFJ6UHpUbC9Xak1zPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJjb25xdWVzdC1wdWJsaWMtdWF0LXN1bmxpZmUtand0LWludGVncmF0aW9uIiwiYXVkaXRUcmFja2luZ0lkIjoiM2UwNjc5ODYtMzRmYy00MzhjLThiYmEtOWJlODhiNGUzZTgxIiwiaXNzIjoiaHR0cHM6Ly9zdW5saWZlLWF1dGgudWF0LmNvbnF1ZXN0LXB1YmxpYy5jb25xdWVzdHBsYW5uaW5nLmNvbTo0NDMvbG9naW4vb2F1dGgyL3JlYWxtcy9yb290L3JlYWxtcy9jb24vcmVhbG1zL3VhdCIsInRva2VuTmFtZSI6ImFjY2Vzc190b2tlbiIsInR5cCI6IkJlYXJlciIsInRva2VuX3R5cGUiOiJCZWFyZXIiLCJhdXRoR3JhbnRJZCI6IjhmODVjOTFkLTQ1NzAtNDA5Ni1iYTdkLWI3Mzk2NDJiZGVhMiIsImF1ZCI6ImNvbnF1ZXN0LXB1YmxpYy11YXQtc3VubGlmZS1qd3QtaW50ZWdyYXRpb24iLCJuYmYiOjE2NjYyOTg1OTksInJlYWxtX2FjY2VzcyI6e30sInNjb3BlIjoiYXBpLmNvbnF1ZXN0cGxhbm5pbmcuY29tIiwiYXV0aF90aW1lIjoxNjY2Mjk4NTk5LCJyZWFsbSI6Ii9jb24vdWF0IiwiZXhwIjoxNjY2MzAwMzk5LCJpYXQiOjE2NjYyOTg1OTksImV4cGlyZXNfaW4iOjE4MDAwMDAsImp0aSI6IjU0ZjMzYzU2LTRhYjktNGI2OC1hYWU2LTAwZGJhZWJiNmVhOSJ9.Fvp2bs2h4pRo9Dcd_w7yMJGwY0Acq4h1fouYbo6b0WVVu8KTTC3Xxrl59kPT7f8Rsd-BjeORM83VypgAVWBvEhZWSOY_PpEIgPL0_EHBDOsOyd9x6Q_78WtVxpQ37Vag3nGT_EZA2b5ECWX1fg4C0qIJ4uUf4wyI6a91fui-95EgVBRsdsNa7TaX4AcsCX4T_96X-sqUY127YGyKV20S9ppKzwpg2kR1Xp43_HxtyBu5i-oSj8ry1EVZd5I0hTl2dzddyYUT8SfCiitS-BrAXC_1MM91td00kn3WlMjFahE5PcC6rg8yVFGpG0OQyIbElvCnfSeqNLjx3FPyVx3rqw","scope":"api.conquestplanning.com","token_type":"Bearer","expires_in":1799}
+            System.out.println(response.statusCode() + " " + response.body().toString());
+            if(response.statusCode() == 200) {
+                // construct a token response and return it.
+                Map<String, Object> map = JsonMapper.string2Map(response.body().toString());
+                if(map != null) {
+                    tokenResponse = new TokenResponse();
+                    tokenResponse.setAccessToken((String)map.get("access_token"));
+                    tokenResponse.setTokenType((String)map.get("token_type"));
+                    tokenResponse.setScope((String)map.get("scope"));
+                    tokenResponse.setExpiresIn((Integer)map.get("expires_in"));
+                    return tokenResponse;
+                } else {
+                    logger.error("response body cannot be parsed as a JSON " + response.body());
+                    return null;
+                }
+            } else {
+                logger.error("Error in getting the token with status code " + response.statusCode() + " and body " + response.body().toString());
+                return null;
+            }
+        } catch (Exception e) {
+            logger.error("Exception:", e);
+            return null;
+        }
+    }
+}
--- soap-security/README.md
+++ soap-security/README.md
@@ -2,9 +2,140 @@ This module contains a rule action implementation to inject security section int
 
 One of our customers is accessing a Cannex Soap API from the corporate network with light-gateway via the generic [ExternalServiceHandler](/concern/external-handler/). For all the consumers, they don't need to set the security header in the SOAP request; the light-gateway intercepts the request body and adds security header with the correct credentials to access the Cannex. This is very similar to the above REST to SOAP request transformer; however, it is a little bit complicated as we are accessing an external service instead of internal services. 
 
-For detailed configuration, please review this [PR](https://github.com/networknt/light-gateway/pull/69)
+### Download Jar
 
-Also, the following is a video walk through. 
+The plugin jar file can be downloaded at this location for the snapshot version. 
+
+https://oss.sonatype.org/service/local/repositories/snapshots/content/com/networknt/soap-security/2.1.3-SNAPSHOT/soap-security-2.1.3-20221031.175118-1.jar
+
+The released jar will be added later but the codebase should be the same. 
+
+
+### Configuration: 
+
+In the rules.yml file, add the following rule.
+
+```
+soap-security-request:
+  ruleId: soap-security-request
+  host: lightapi.net
+  ruleType: request-transform
+  visibility: public
+  description: Transform the request body to add the security section into the header for soap API call for external service.
+  conditions:
+    - conditionId: path-soap
+      propertyPath: requestPath
+      operatorCode: EQ
+      joinCode: AND
+      index: 1
+      conditionValues:
+        - conditionValueId: path
+          conditionValue: /devext/CANX/AntcMultiService
+  actions:
+    - actionId: soap-request-transform
+      actionClassName: com.networknt.soap.SoapSecurityTransformAction
+
+```
+
+This rule will be triggered when the request path matches the condition and the action class will be invoked to add the securit header in the SOAP reqeust body. 
+
+In the values.yml file, we need to do that following changes to overwrite several config properties. 
+
+cannex.yml
+
+```
+# cannex.yml
+cannex.username: SLUATWS
+
+```
+You can also add the cannex.password: real-password into the values.yml file; however, I have added it into an environment variable in .profile file. 
+
+```
+export CANNEX_PASSWORD=real-password
+```
+
+external-service.yml
+```
+externalService.pathHostMappings:
+  - /devext/CANX/AntcMultiService https://wwwdev.cannex.com
+```
+
+Add the request path and the external host mapping to the externalService.pathHostMappings
+
+
+body.yml
+
+```
+# body.yml
+body.cacheRequestBody: true
+body.cacheResponseBody: true
+
+```
+
+Update the body handler to allow the request body and repsonse body to be cached so that they can be populated into the audit log.
+
+
+handler.yml
+
+```
+handler.handlers:
+  .
+  .
+  .
+  - com.networknt.handler.ResponseInterceptorInjectionHandler@responseInterceptor
+  - com.networknt.handler.RequestInterceptorInjectionHandler@requestInterceptor
+  .
+  .
+  .
+
+handler.chains.default:
+  .
+  .
+  .
+  - header
+  - requestInterceptor
+  - responseInterceptor
+  .
+  .
+  .
+```
+
+service.yml
+
+```
+  - com.networknt.handler.RequestInterceptor:
+      - com.networknt.reqtrans.RequestTransformerInterceptor
+      - com.networknt.body.RequestBodyInterceptor
+
+```
+
+Above add the RequestTransformerInterceptor and RequestBodyInterceptor to the default chain via requestInterceptor defined in the handler.yml file.
+
+Please note that RequestBodyInterceptor must be the last interceptor in the interceptor list. 
+
+request-transformer.yml
+
+```
+# request-transformer.yml
+request-transformer.appliedPathPrefixes: ["/pets","/v1/flowers","/devext/CANX/AntcMultiService"]
+
+```
+
+Add the request path prefix to the request-transformer.appliedPathPrefixes list. 
+
+rule-loader.yml
+
+```
+rule-loader.endpointRules: {"/v1/pets@get":{"request-transform":[{"ruleId":"petstore-request-header-replace"}],"response-transform":[{"ruleId":"petstore-response-header-replace"}]},"/v1/notifications@get":{"response-transform":[{"ruleId":"petstore-notifications-transformer"}]},"/pets@get":{"request-transform":[{"ruleId":"petstore-request-path"}]},"/v1/flowers@post":{"request-transform":[{"ruleId":"petstore-flower-request"}],"response-transform":[{"ruleId":"petstore-flower-response"}]},"/devext/CANX/AntcMultiService@post":{"request-transform":[{"ruleId":"soap-security-request"}]}}
+
+```
+
+Add the endpoint /devext/CANX/AntcMultiService@post and the ruleId to the mapping.
+
+### Tutorial
+
+
+The following is a video walk through; however, some the configuration might be changed already. Please following the configuration above if you want try it out. 
 
 https://www.youtube.com/watch?v=MTA6Pf1TjaU
 
