ab134d4ccd38a5cf0ed64bc0f168da30eeb9c31d
Steve Hu <stevehu@gmail.com>
2023-08-10 19:01:08 -0400

    fixes #51 update the epam token to return the token instead of injection (#52)

--- epam-token/src/main/java/com/networknt/rule/epam/EpamTokenRequestTransformAction.java
+++ epam-token/src/main/java/com/networknt/rule/epam/EpamTokenRequestTransformAction.java
@@ -11,6 +11,7 @@ import com.networknt.proxy.PathPrefixAuth;
 import com.networknt.rule.IAction;
 import com.networknt.rule.RuleActionValue;
 import com.networknt.rule.RuleConstants;
+import com.networknt.status.Status;
 import com.networknt.utility.HashUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,15 +34,14 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * It is called from the request transform interceptor from the light-gateway to get the conquest planning API access
- * token to put into the Authorization header in order to access the conquest planning APIs. For the original consumer,
- * it might have another token in the Authorization header for the gateway to verify in order to invoke the external
- * service handler. Once the verification is done, the Authorization header will be replaced with the conquest token
- * from the cache or retrieved from the conquest if it is expired or about to expire.
+ * It is called from the request transform interceptor from the light-gateway to get the e-PAM API access token with
+ * the passed in grant_type, client_id and scope. The plugin will create a brand new SSLContext with keystore to
+ * enable authentication with PING OAuth server.
  *
  * @author Steve Hu
  */
 public class EpamTokenRequestTransformAction implements IAction {
+    public static final String GET_TOKEN_ERROR = "ERR10052";
     private static final Logger logger = LoggerFactory.getLogger(EpamTokenRequestTransformAction.class);
     // change the config to static so that it can cache the token retrieved until expiration time.
     private static final EpamConfig config = EpamConfig.load();
@@ -56,27 +56,43 @@ public class EpamTokenRequestTransformAction implements IAction {
         resultMap.put(RuleConstants.RESULT, true);
         String requestPath = (String)objMap.get("requestPath");
         if(logger.isTraceEnabled()) logger.trace("requestPath = " + requestPath);
+
+
         for(PathPrefixAuth pathPrefixAuth: config.getPathPrefixAuths()) {
             if(requestPath.startsWith(pathPrefixAuth.getPathPrefix())) {
                 if(logger.isTraceEnabled()) logger.trace("found with requestPath = " + requestPath + " prefix = " + pathPrefixAuth.getPathPrefix());
-                if(System.currentTimeMillis() >= (pathPrefixAuth.getExpiration())) {
+                if(System.currentTimeMillis() >= pathPrefixAuth.getExpiration()) {
                     if(logger.isTraceEnabled()) logger.trace("Cached token {} is expired with current time {} and expired time {}", pathPrefixAuth.getAccessToken() != null ? pathPrefixAuth.getAccessToken().substring(0, 20) : null, System.currentTimeMillis(), pathPrefixAuth.getExpiration());
-                    TokenResponse tokenResponse = getAccessToken(pathPrefixAuth.getTokenUrl(), pathPrefixAuth.getClientId(), pathPrefixAuth.getScope());
+                    String requestBody = (String)objMap.get("requestBody");
+                    if(logger.isTraceEnabled()) logger.trace("requestBody = " + requestBody);
+                    Map<String, Object> requestMap = JsonMapper.string2Map(requestBody);
+                    String clientId = (String)requestMap.get("client_id");
+                    String scope = (String)requestMap.get("scope");
+                    TokenResponse tokenResponse = getAccessToken(pathPrefixAuth.getTokenUrl(), clientId, scope);
                     if(tokenResponse != null) {
                         pathPrefixAuth.setExpiration(System.currentTimeMillis() + tokenResponse.getExpiresIn() * 1000 - 60000);
                         pathPrefixAuth.setAccessToken(tokenResponse.getAccessToken());
                         if(logger.isTraceEnabled()) logger.trace("Got a new token {} and cached it with expiration time {}", pathPrefixAuth.getAccessToken() != null ? pathPrefixAuth.getAccessToken().substring(0, 20) : null, pathPrefixAuth.getExpiration());
                     } else {
+                        if(logger.isTraceEnabled()) logger.trace("Failed to get a new token");
+                        // need to return an error message to the caller.
+                        resultMap.put("contentType", "application/json");
+                        Status status = new Status(GET_TOKEN_ERROR);
+                        resultMap.put("statusCode", status.getStatusCode());
+                        resultMap.put("responseBody", status.toString());
                         return;
                     }
                 }
                 // either a new token is retrieved or cached token is not expired. Put the token into the Authorization header.
                 if(pathPrefixAuth.getAccessToken() != null) {
-                    Map<String, Object> requestHeaders = new HashMap<>();
-                    Map<String, Object> updateMap = new HashMap<>();
-                    updateMap.put("Authorization", "Bearer " + pathPrefixAuth.getAccessToken());
-                    requestHeaders.put("update", updateMap);
-                    resultMap.put("requestHeaders", requestHeaders);
+                    resultMap.put("contentType", "application/json");
+                    resultMap.put("statusCode", 200);
+                    // construct the response body
+                    Map<String, Object> responseBody = new HashMap<>();
+                    responseBody.put("access_token", pathPrefixAuth.getAccessToken());
+                    responseBody.put("token_type", "Bearer");
+                    responseBody.put("expires_in", (pathPrefixAuth.getExpiration() - System.currentTimeMillis()) / 1000);
+                    resultMap.put("responseBody", JsonMapper.toJson(responseBody));
                     return;
                 }
             }
