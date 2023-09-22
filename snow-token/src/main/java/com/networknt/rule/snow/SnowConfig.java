c5b4535cc0809f6132198926b0d40cc44420ff21
Steve Hu <stevehu@gmail.com>
2023-09-22 14:40:35 -0400

    fixes #58 Add ServiceNow token module

---
+++ snow-token/src/main/java/com/networknt/rule/snow/SnowConfig.java
@@ -0,0 +1,163 @@
+package com.networknt.rule.snow;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.networknt.config.Config;
+import com.networknt.config.ConfigException;
+import com.networknt.proxy.PathPrefixAuth;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class SnowConfig {
+    private static final Logger logger = LoggerFactory.getLogger(SnowConfig.class);
+
+    public static final String CONFIG_NAME = "snow";
+    public static final String PATH_PREFIX = "pathPrefix";
+    public static final String TOKEN_URL = "tokenUrl";
+    public static final String TOKEN_TTL = "tokenTtl";
+    public static final String PROXY_HOST = "proxyHost";
+    public static final String PROXY_PORT = "proxyPort";
+    public static final String ENABLE_HTTP2 = "enableHttp2";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String CLIENT_ID = "client_id";
+    public static final String CLIENT_SECRET = "client_secret";
+    public static final String GRANT_TYPE = "grant_type";
+    public static final String PATH_PREFIX_AUTHS = "pathPrefixAuths";
+
+    String proxyHost;
+    int proxyPort;
+    boolean enableHttp2;
+
+    List<PathPrefixAuth> pathPrefixAuths;
+    private final Config config;
+    private Map<String, Object> mappedConfig;
+
+    private SnowConfig() {
+        this(CONFIG_NAME);
+    }
+
+    /**
+     * Please note that this constructor is only for testing to load different config files
+     * to test different configurations.
+     * @param configName String
+     */
+    private SnowConfig(String configName) {
+        config = Config.getInstance();
+        mappedConfig = config.getJsonMapConfigNoCache(configName);
+        setConfigData();
+        setConfigList();
+    }
+    public static SnowConfig load() {
+        return new SnowConfig();
+    }
+
+    public static SnowConfig load(String configName) {
+        return new SnowConfig(configName);
+    }
+
+    void reload() {
+        mappedConfig = config.getJsonMapConfigNoCache(CONFIG_NAME);
+        setConfigData();
+        setConfigList();
+    }
+
+    public Map<String, Object> getMappedConfig() {
+        return mappedConfig;
+    }
+
+    public void setMappedConfig(Map<String, Object> mappedConfig) {
+        this.mappedConfig = mappedConfig;
+    }
+
+    public String getProxyHost() {
+        return proxyHost;
+    }
+
+    public void setProxyHost(String proxyHost) {
+        this.proxyHost = proxyHost;
+    }
+
+    public int getProxyPort() {
+        return proxyPort;
+    }
+
+    public void setProxyPort(int proxyPort) {
+        this.proxyPort = proxyPort;
+    }
+
+    public boolean isEnableHttp2() {
+        return enableHttp2;
+    }
+
+    public void setEnableHttp2(boolean enableHttp2) {
+        this.enableHttp2 = enableHttp2;
+    }
+
+    public List<PathPrefixAuth> getPathPrefixAuths() {
+        return pathPrefixAuths;
+    }
+
+    public void setPathPrefixAuths(List<PathPrefixAuth> pathPrefixAuths) {
+        this.pathPrefixAuths = pathPrefixAuths;
+    }
+
+    private void setConfigData() {
+        Object object = mappedConfig.get(PROXY_HOST);
+        if(object != null) {
+            setProxyHost((String) object);
+        }
+        object = mappedConfig.get(PROXY_PORT);
+        if (object != null) {
+            setProxyPort((int) object);
+        }
+        object = mappedConfig.get(ENABLE_HTTP2);
+        if(object != null && (Boolean) object) {
+            setEnableHttp2(true);
+        }
+    }
+
+    private void setConfigList() {
+        // path prefix auth mapping
+        if (mappedConfig.get(PATH_PREFIX_AUTHS) != null) {
+            Object object = mappedConfig.get(PATH_PREFIX_AUTHS);
+            pathPrefixAuths = new ArrayList<>();
+            if(object instanceof String) {
+                String s = (String)object;
+                s = s.trim();
+                if(logger.isTraceEnabled()) logger.trace("pathPrefixAuth s = " + s);
+                if(s.startsWith("[")) {
+                    // json format
+                    try {
+                        pathPrefixAuths = Config.getInstance().getMapper().readValue(s, new TypeReference<List<PathPrefixAuth>>() {});
+                    } catch (Exception e) {
+                        throw new ConfigException("could not parse the pathPrefixAuth json with a list of string and object.");
+                    }
+                } else {
+                    throw new ConfigException("pathPrefixAuth must be a list of string object map.");
+                }
+            } else if (object instanceof List) {
+                // the object is a list of map, we need convert it to PathPrefixAuth object.
+                List<Map<String, Object>> values = (List<Map<String, Object>>)object;
+                for(Map<String, Object> value: values) {
+                    PathPrefixAuth pathPrefixAuth = new PathPrefixAuth();
+                    pathPrefixAuth.setPathPrefix((String)value.get(PATH_PREFIX));
+                    pathPrefixAuth.setTokenTtl((Integer)value.get(TOKEN_TTL));
+                    pathPrefixAuth.setTokenUrl((String)value.get(TOKEN_URL));
+                    pathPrefixAuth.setUsername((String)value.get(USERNAME));
+                    pathPrefixAuth.setPassword((String)value.get(PASSWORD));
+                    pathPrefixAuth.setClientId((String)value.get(CLIENT_ID));
+                    pathPrefixAuth.setClientSecret((String)value.get(CLIENT_SECRET));
+                    pathPrefixAuth.setGrantType((String)value.get(GRANT_TYPE));
+                    pathPrefixAuths.add(pathPrefixAuth);
+                }
+            } else {
+                throw new ConfigException("pathPrefixAuth must be a list of string object map.");
+            }
+        }
+    }
+
+}
---
+++ snow-token/src/main/java/com/networknt/rule/snow/SnowTokenRequestTransformAction.java
@@ -0,0 +1,152 @@
+package com.networknt.rule.snow;
+
+import com.networknt.client.ClientConfig;
+import com.networknt.client.Http2Client;
+import com.networknt.client.oauth.TokenResponse;
+import com.networknt.client.ssl.TLSConfig;
+import com.networknt.config.Config;
+import com.networknt.config.JsonMapper;
+import com.networknt.proxy.PathPrefixAuth;
+import com.networknt.rule.IAction;
+import com.networknt.rule.RuleActionValue;
+import com.networknt.rule.RuleConstants;
+import com.networknt.utility.ModuleRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * It is called from the request transform interceptor from the light-gateway to get the password grant type ServiceNow token
+ * to replace the JWT token that consumers use to access the gateway. This allows the ServiceNow API to be exposed on the
+ * Gateway as a standard API with client credentials token for API to API invocations. 
+ *
+ * @author Steve Hu
+ */
+public class SnowTokenRequestTransformAction implements IAction {
+    private static final Logger logger = LoggerFactory.getLogger(SnowTokenRequestTransformAction.class);
+    // change the config to static so that it can cache the token retrieved until expiration time.
+    private static final SnowConfig config = SnowConfig.load();
+    private static HttpClient client;
+
+    public SnowTokenRequestTransformAction() {
+        if(logger.isInfoEnabled()) logger.info("SnowTokenRequestTransformAction is constructed");
+        List<String> masks = new ArrayList<>();
+        masks.add("password");
+        masks.add("client_secret");
+        ModuleRegistry.registerModule(SnowTokenRequestTransformAction.class.getName(), Config.getInstance().getJsonMapConfigNoCache(SnowConfig.CONFIG_NAME), masks);
+    }
+
+    @Override
+    public void performAction(Map<String, Object> objMap, Map<String, Object> resultMap, Collection<RuleActionValue> actionValues) {
+        resultMap.put(RuleConstants.RESULT, true);
+        String requestPath = (String)objMap.get("requestPath");
+        if(logger.isTraceEnabled()) logger.trace("requestPath = " + requestPath);
+
+        for(PathPrefixAuth pathPrefixAuth: config.getPathPrefixAuths()) {
+            if(requestPath.startsWith(pathPrefixAuth.getPathPrefix())) {
+                if(logger.isTraceEnabled()) logger.trace("found with requestPath = " + requestPath + " prefix = " + pathPrefixAuth.getPathPrefix());
+                if(System.currentTimeMillis() >= pathPrefixAuth.getExpiration()) {
+                    if(logger.isTraceEnabled()) logger.trace("Cached token {} is expired with current time {} and expired time {}", pathPrefixAuth.getAccessToken() != null ? pathPrefixAuth.getAccessToken().substring(0, 20) : null, System.currentTimeMillis(), pathPrefixAuth.getExpiration());
+                    TokenResponse tokenResponse = getAccessToken(pathPrefixAuth.getTokenUrl(), pathPrefixAuth.getUsername(), pathPrefixAuth.getPassword(), pathPrefixAuth.getClientId(), pathPrefixAuth.getClientSecret(), pathPrefixAuth.getGrantType());
+                    if(tokenResponse != null) {
+                        pathPrefixAuth.setExpiration(System.currentTimeMillis() + tokenResponse.getExpiresIn() * 1000 - 60000);
+                        pathPrefixAuth.setAccessToken(tokenResponse.getAccessToken());
+                        if(logger.isTraceEnabled()) logger.trace("Got a new token {} and cached it with expiration time {}", pathPrefixAuth.getAccessToken() != null ? pathPrefixAuth.getAccessToken().substring(0, 20) : null, pathPrefixAuth.getExpiration());
+                    } else {
+                        if(logger.isTraceEnabled()) logger.trace("tokenResponse is null");
+                        return;
+                    }
+                }
+                // either a new token is retrieved or cached token is not expired. Put the token into the Authorization header.
+                if(pathPrefixAuth.getAccessToken() != null) {
+                    Map<String, Object> requestHeaders = new HashMap<>();
+                    Map<String, Object> updateMap = new HashMap<>();
+                    updateMap.put("Authorization", "Bearer " + pathPrefixAuth.getAccessToken());
+                    requestHeaders.put("update", updateMap);
+                    resultMap.put("requestHeaders", requestHeaders);
+                    return;
+                }
+            }
+        }
+    }
+
+    private TokenResponse getAccessToken(String serverUrl, String username, String password, String clientId, String clientSecret, String grantType) {
+        TokenResponse tokenResponse = null;
+        if(client == null) {
+            try {
+                HttpClient.Builder clientBuilder = HttpClient.newBuilder()
+                        .followRedirects(HttpClient.Redirect.NORMAL)
+                        .connectTimeout(Duration.ofMillis(ClientConfig.get().getTimeout()))
+                        .sslContext(Http2Client.createSSLContext());
+                if(config.getProxyHost() != null) clientBuilder.proxy(ProxySelector.of(new InetSocketAddress(config.getProxyHost(), config.getProxyPort() == 0 ? 443 : config.getProxyPort())));
+                if(config.isEnableHttp2()) clientBuilder.version(HttpClient.Version.HTTP_2);
+                // this a workaround to bypass the hostname verification in jdk11 http client.
+                Map<String, Object> tlsMap = (Map<String, Object>)ClientConfig.get().getMappedConfig().get(Http2Client.TLS);
+                if(tlsMap != null && !Boolean.TRUE.equals(tlsMap.get(TLSConfig.VERIFY_HOSTNAME))) {
+                    final Properties props = System.getProperties();
+                    props.setProperty("jdk.internal.httpclient.disableHostnameVerification", Boolean.TRUE.toString());
+                }
+                client = clientBuilder.build();
+            } catch (IOException e) {
+                logger.error("Cannot create HttpClient:", e);
+                return null;
+            }
+        }
+        try {
+            if(serverUrl == null) {
+                logger.error("tokenUrl is null");
+                return null;
+            }
+
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("username", username);
+            parameters.put("password", password);
+            parameters.put("client_id", clientId);
+            parameters.put("client_secret", clientSecret);
+            parameters.put("grant_type", grantType);
+
+            String form = parameters.entrySet()
+                    .stream()
+                    .map(e -> e.getKey() + "=" + URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8))
+                    .collect(Collectors.joining("&"));
+
+            if(logger.isTraceEnabled()) logger.trace("request body = " + form);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(serverUrl))
+                    .headers("Content-Type", "application/x-www-form-urlencoded")
+                    .POST(HttpRequest.BodyPublishers.ofString(form))
+                    .build();
+
+            HttpResponse<?> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            // {"access_token":"kldsfoo4...","refresh_token: "390343...", "scope": "useraccount","token_type":"Bearer", "expires_in":1079}
+            if(response.statusCode() == 200) {
+                // construct a token response and return it.
+                Map<String, Object> map = JsonMapper.string2Map(response.body().toString());
+                if(map != null) {
+                    tokenResponse = new TokenResponse();
+                    tokenResponse.setAccessToken((String)map.get("access_token"));
+                    tokenResponse.setExpiresIn(((Integer)map.get("expires_in")));
+                    return tokenResponse;
+                } else {
+                    logger.error("response body cannot be parsed as a JSON " + response.body());
+                    return null;
+                }
+            } else {
+                logger.error("Error in getting the token with status code " + response.statusCode() + " and body " + response.body().toString());
+                return null;
+            }
+        } catch (Exception e) {
+            logger.error("Exception:", e);
+            return null;
+        }
+    }
+}
